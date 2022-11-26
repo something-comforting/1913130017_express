bc2a0fe1354a8820ccb73a68b7037283364e68ad
something-comforting <118034429+something-comforting@users.noreply.github.com>
2022-11-26 11:26:56 +0700

    ✍️ch3: workshopA

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const bio = {
+  fullname: 'Thitiwat Teeramessiriyos',
+  nickname: 'Unn',
+  hobby: ['Sleep', 'Play games'],
+  gitusername: 'something-comforting'
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res, next) => {
+  res.status(200).json({ fullname: 'Thitiwat Teeramessiriyos' })
+})
 
-module.exports = router;
+router.get('/bio', (req, res) => {
+  res.json(bio)
+})
+
+module.exports = router
